91d74cadcba7ba4fa042aa10fc0221ba42e1b6bc
Дмитрий <osdal67@gmail.com>
2023-09-02 12:40:43 +0300

    Величина шрифта зависит от размеров блока

--- js/my.js
+++ js/my.js
@@ -11,13 +11,18 @@ window.addEventListener("load", (event) => {
 	let finalName = wrappedName.join('');
 	name.innerHTML = finalName;
 
+	const element = document.querySelector('.logo_container');
+	const width = element.clientWidth;
+	const height = element.clientHeight;
+	console.log(`Ширина: ${width}px, Высота: ${height}px`);
 
 	let spanElements = name.getElementsByTagName('span');
-
+	let fs = width * 0.12;
 
 	for (i = 0; i < spanElements.length; i++) {
 		spanElements[i].style.transform = "rotate(" + (i + 1) * 360 / spanElements.length + "deg)";
 	}
+
 	for (i = 0; i < spanElements.length; i++) {
 		if (spanElements[i].innerHTML == ' ') {
 			spanElements[i].classList.add('fa-regular', 'fa-star-of-david');
@@ -26,35 +31,31 @@ window.addEventListener("load", (event) => {
 		}
 	}
 
-	const element = document.querySelector('.logo_container');
-	const width = element.clientWidth;
-	const height = element.clientHeight;
-	console.log(`Ширина: ${width}px, Высота: ${height}px`);
+
 
 	let image = document.querySelector('.round-image-container');
-	image.style.width = image.style.height = width * 1.1 + 'px';
+	image.style.width = image.style.height = width * 0.8 + 'px';
 	console.log(image.style.width);
+	for (i = 0; i < spanElements.length; i++) {
+		spanElements[i].style.fontSize = fs + 'px';
+	}
+
+	function increaseFont() {
+		var textElement = document.getElementById("myText");
+		var currentFontSize = window.getComputedStyle(textElement, null).getPropertyValue('font-size');
+		var newSize = parseFloat(currentFontSize) * 1.2; // Увеличение на 20%
+		textElement.style.fontSize = newSize + "px";
+	}
+
+	function decreaseFont() {
+		var textElement = document.getElementById("myText");
+		var currentFontSize = window.getComputedStyle(textElement, null).getPropertyValue('font-size');
+		var newSize = parseFloat(currentFontSize) * 0.8; // Уменьшение на 20%
+		textElement.style.fontSize = newSize + "px";
+	}
 
 
 
-
-
-
-
-	// Получаем элемент, содержащий текст
-	// const textContainer = document.getElementById('textContainer');
-
-	// Получаем текст из элемента
-	// const text = textContainer.textContent;
-
-	// Разбиваем текст на отдельные буквы
-	// const letters = text.split('');
-
-	// Создаем новый HTML-код, оборачивая каждую букву в span
-	// const wrappedText = letters.map(letter => `<span>${letter}</span>`).join('');
-
-	// Вставляем обернутый текст обратно в элемент
-	// textContainer.innerHTML = wrappedText;
 
 
 });
